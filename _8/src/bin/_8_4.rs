497a31cfab7428464cea0f6e2458cf51251764a9
aris <slut@vip.qq.com>
2022-09-02 17:39:23 +0800

    8.4

--- _8/src/bin/_8_4.rs
+++ _8/src/bin/_8_4.rs
@@ -22,4 +22,33 @@ fn main() {
             println!("{}", n);
         }
     }
+
+    //iter 在每次迭代中,借用集合中的一个元素
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    println!("{:?}", names);
+
+    //into_iter 会消耗集合,一旦集合被消耗,之后就无法使用
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    //该语句无法执行 println!("{:?}", names);
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);
 }
