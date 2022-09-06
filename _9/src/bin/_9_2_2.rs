a0ee01af7df331182441b6eb78b9ebe12b6c281d
aris <slut@vip.qq.com>
2022-09-06 11:26:14 +0800

    9.2.2

---
+++ _9/src/bin/_9_2_2.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let greeting = "hello";
+    let mut farewell = "goodbye".to_owned();
+
+    let diary = || {
+        // `greeting` 通过引用捕获，故需要闭包是 `Fn`。
+        println!("I said {}.", greeting);
+
+        // 下文改变了 `farewell` ，因而要求闭包通过可变引用来捕获它。
+        // 现在需要 `FnMut`。
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // 手动调用 drop 又要求闭包通过值获取 `farewell`。
+        // 现在需要 `FnOnce`。
+        std::mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| 2 * x;
+    println!("3 doubled: {}", apply_to_3(double));
+}
+
+fn apply<F>(f: F)
+where
+    F: FnOnce(),
+{
+    f();
+}
+
+fn apply_to_3<F>(f: F) -> i32
+where
+    F: Fn(i32) -> i32,
+{
+    f(3)
+}
