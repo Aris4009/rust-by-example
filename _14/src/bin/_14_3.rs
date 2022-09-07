edb059b41b99219abe8e62e9c57b92b4366ca5ad
aris <slut@vip.qq.com>
2022-09-07 17:31:24 +0800

    14.3

---
+++ _14/src/bin/_14_3.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let empty = Empty;
+    let null = Null;
+
+    empty.double_drop(null);
+
+    // empty;
+    // null;
+}
+
+struct Empty;
+
+struct Null;
+
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+}
+
+impl<T, U> DoubleDrop<T> for U {
+    fn double_drop(self, _: T) {}
+}
