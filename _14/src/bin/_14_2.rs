caf3b3f0533220a1a854ee643d16d66f25d1fbd7
aris <slut@vip.qq.com>
2022-09-07 17:21:24 +0800

    14.2

---
+++ _14/src/bin/_14_2.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{}, {}", x.value(), y.value());
+}
+
+struct Val {
+    val: f64,
+}
+
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
